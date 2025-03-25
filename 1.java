4d54038427f275aeeeb8db1e9aa781c56acd6a4f
praharshamaroju <marojupraharsha@gmail.com>
2025-03-25 12:08:49 +0530

    Add files via upload
    test filescode

---
+++ 1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner scan =new Scanner(System.in);
+        int n = scan.nextInt();
+        int i,j;
+        for(i=1;i<=n;i++){
+            for(j=1;j<=i;j++){
+                System.out.print( "* ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ 3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class RightAlignedTriangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int n = scanner.nextInt();
+        scanner.close();
+
+        for (int i = 1; i <= n; i++) {
+            // Print spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+ }
+}
+}
